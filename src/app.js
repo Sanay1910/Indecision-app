1ba24c4c1d89bb2dc765504c6f5bbd2c197278fb
Sanay1910 <sanay1910@gmail.com>
2020-03-23 20:09:57 +0100

    Chapter 12 completed, explored and operator and ternary operator for conditional renderings.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,49 @@
 console.log('App.js is running');
 
+//if statements
+// ternary operators
+// logical and operator
+
+var app = {
+    title: 'Indecision App',
+    subtitle: 'This is some info',
+    options: ['One', 'Two']
+}
+
 // JSX - JavaScript XML
-var template = <h1>Does this change!</h1>;
+var template = (
+    <div>
+        <h1>{app.title}</h1>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+var user = {
+    name: 'Sanay',
+    age: 23,
+    location: 'Oslo'
+}
+
+function getLocation(location) {
+    if(location) {
+        return <p>Location: {location}</p>;
+    }
+}
+
+var templateTwo = (
+    <div>
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
+    </div>
+);
+
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
